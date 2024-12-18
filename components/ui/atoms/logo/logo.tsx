d4b1c99cee1223ef0b9dc986e1c9292796b5d897
Antoine V <antoine.verove@gmail.com>
2024-12-18 11:04:32 +0100

    Ajoute le logo, en cours de construction

---
+++ components/ui/atoms/logo/logo.tsx
@@ -0,0 +1,7 @@
+// import './logo.scss'
+// import Image from 'next/image'
+// import logo from '@/public/logo.png'
+
+// export default function Logo() {
+//     return <Image src={logo} alt="logo" placeholder="blur" />
+// }

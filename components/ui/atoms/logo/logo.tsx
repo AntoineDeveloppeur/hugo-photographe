daa3d9c254117e9fe3ea325500b23edb3d2560df
Antoine V <antoine.verove@gmail.com>
2024-12-18 11:32:00 +0100

    Ajout logo fonctionnel

--- components/ui/atoms/logo/logo.tsx
+++ components/ui/atoms/logo/logo.tsx
@@ -1,7 +1,7 @@
-// import './logo.scss'
-// import Image from 'next/image'
-// import logo from '@/public/logo.png'
+import './logo.css'
+import Image from 'next/image'
+import logo from '@/public/logo.png'
 
-// export default function Logo() {
-//     return <Image src={logo} alt="logo" placeholder="blur" />
-// }
+export default function Logo() {
+    return <Image src={logo} alt="logo" placeholder="blur" />
+}

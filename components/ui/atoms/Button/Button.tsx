af9cece171d0b36eb8e7191548c0ba067be4fd2c
Antoine V <antoine.verove@gmail.com>
2024-12-23 15:47:38 +0100

    Crée button avec et sans text

--- components/ui/atoms/Button/Button.tsx
+++ components/ui/atoms/Button/Button.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { ButtonProps } from '@/types'
+import ArrowRight from '../ArrowRight/ArrowRight'
+import styles from './button.module.scss'
 
 const Button = ({ text }: ButtonProps) => {
     return (
-        <button onClick={() => {}}>
-            <p>{text}</p>
+        <button onClick={() => {}} className={styles.button}>
+            {text && <p className={styles.button__p}>{text}</p>}
+            <ArrowRight />
         </button>
     )
 }

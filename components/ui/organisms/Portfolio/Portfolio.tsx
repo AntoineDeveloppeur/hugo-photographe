2d0d20cc93a600843646a4e44a575e40dbb6a7d3
Antoine V <antoine.verove@gmail.com>
2024-12-23 17:49:53 +0100

    Ajoute la division largeScreen aux Organismes Portfolio et ProjetSection. Ajoute le top de la section ProjetSection

--- components/ui/organisms/Portfolio/Portfolio.tsx
+++ components/ui/organisms/Portfolio/Portfolio.tsx
@@ -4,36 +4,28 @@ import Subtitle from '../../atoms/Subtitle/Subtitle'
 import data from '@/data/data.json'
 import Photo from '../../atoms/Photo/Photo'
 
-// export default function () {
-//     return (
-//         <div className={styles.portfolio}>
-//             <Title content="PORTFOLIO" />
-//             <div className={styles.subtitleWrapper}>
-//                 <Subtitle content="UN APERCU DE MON TRAVAIL" />
-//             </div>
-//             <PhotosRows rows={data.galery} />
-//         </div>
-//     )
-// }
-
 export default function Portfolio() {
     return (
         <section className={styles.portfolio}>
-            <Title text="PORTFOLIO" />
-            <div className={styles.portfolio__subtitleWrapper}>
-                <Subtitle text="UN APERCU DE MON TRAVAIL" />
-            </div>
-            <div className={styles.portfolio__columns}>
-                {data.galery.map((columns, i): any => (
-                    <div
-                        key={`column${i}`}
-                        className={styles.portfolio__columns__column}
-                    >
-                        {columns.map((pic, i): any => (
-                            <Photo key={`pic${i}`} photo={pic} />
-                        ))}
-                    </div>
-                ))}
+            <div className={styles.portfolio__largeScreen}>
+                <Title text="PORTFOLIO" />
+                <div className={styles.portfolio__largeScreen__subtitleWrapper}>
+                    <Subtitle text="UN APERCU DE MON TRAVAIL" />
+                </div>
+                <div className={styles.portfolio__largeScreen__columns}>
+                    {data.galery.map((columns, i): any => (
+                        <div
+                            key={`column${i}`}
+                            className={
+                                styles.portfolio__largeScreen__columns__column
+                            }
+                        >
+                            {columns.map((pic, i): any => (
+                                <Photo key={`pic${i}`} photo={pic} />
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     )
--- components/ui/organisms/ProjetSection/ProjetSection.tsx
+++ components/ui/organisms/ProjetSection/ProjetSection.tsx
@@ -1,3 +1,5 @@
+import Subtitle from '../../atoms/Subtitle/Subtitle'
+import Title from '../../atoms/Title/Title'
 import styles from './projet-section.module.scss'
 import Button from '@/components/ui/atoms/Button/Button'
 
@@ -5,8 +7,17 @@ export default function ProjetSection() {
     return (
         <section className={styles.projetSection}>
             <div className={styles.projetSection__largeScreen}>
-                <Button text="Tous les projets" />
-                <Button />
+                <div className={styles.projetSection__largeScreen__top}>
+                    <div
+                        className={
+                            styles.projetSection__largeScreen__top__TitleSubtitle
+                        }
+                    >
+                        <Title text="PROJETS" />
+                        <Subtitle text="VOYAGEZ A TRAVERS MES PROJETS" />
+                    </div>
+                    <Button text="Tous les projets" />
+                </div>
             </div>
         </section>
     )

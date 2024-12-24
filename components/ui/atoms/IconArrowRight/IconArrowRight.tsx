afeecf8a2587a717432a34befa1fa075697a57ba
Antoine V <antoine.verove@gmail.com>
2024-12-24 17:48:43 +0100

    fini la première version de contact, prête pour les test d'animation

--- components/ui/atoms/IconArrowRight/IconArrowRight.tsx
+++ components/ui/atoms/IconArrowRight/IconArrowRight.tsx
@@ -1,4 +1,4 @@
-const ArrowRight = () => {
+const IconArrowRight = () => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -18,4 +18,4 @@ const ArrowRight = () => {
     )
 }
 
-export default ArrowRight
+export default IconArrowRight
--- components/ui/organisms/Contact/Contact.jsx
+++ components/ui/organisms/Contact/Contact.jsx
@@ -1,13 +1,78 @@
 import styles from './contact.module.scss'
 import Button from '../../atoms/Button/Button'
+import Photo from '../../atoms/Photo/Photo'
+import voiture from '@/public/voiture.jpg'
+import Title from '../../atoms/Title/Title'
+import Link from 'next/link'
+import IconMail from '../../atoms/IconMail/IconMail'
+import IconMap from '../../atoms/IconMap/IconMap'
+import IconPhone from '../../atoms/IconPhone/IconPhone'
+import { motion } from 'motion/react'
 const Contact = () => {
-    return (
-        <div className={styles.contact}>
-            <Button text="DECOUVREZ LE PHOTOGRAPHE" />
-            <div className=>
+    const example = { src: voiture.src, alt: 'Voiture' }
 
+    return (
+        <section className={styles.contact}>
+            <Button text="DECOUVREZ LE PHOTOGRAPHE" />
+            <motion.div
+                animate={{
+                    scale: 2,
+                    transition: { duration: 2 },
+                }}
+            />
+            <div className={styles.contact__presentation}>
+                <div className={styles.contact__presentation__photoWrapper}>
+                    <Photo photo={example} />
+                </div>
+                <div className={styles.contact__presentation__text}>
+                    <Title text="HUGO RANDEZ" />
+                    <p className={styles.contact__presentation__text__p}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliim veniam, quis nostrud exercitation ullamc
+                    </p>
+                    <div
+                        className={styles.contact__presentation__text__details}
+                    >
+                        <IconPhone />
+                        <Link
+                            href="tel:+33644210716"
+                            className={
+                                styles.contact__presentation__text__details__link
+                            }
+                        >
+                            06 44 21 07 16
+                        </Link>
+                    </div>
+                    <div
+                        className={styles.contact__presentation__text__details}
+                    >
+                        <IconMail />
+                        <Link
+                            href="mailto:hugo.randez@gmail.com?subject=Demande%20d'information&body=Bonjour Hugo,%20je%20souhaite%20plus%20d'informations%20à%20propos%20de..."
+                            className={
+                                styles.contact__presentation__text__details__link
+                            }
+                        >
+                            hugo.randez@gmail.com
+                        </Link>
+                    </div>
+                    <div
+                        className={styles.contact__presentation__text__details}
+                    >
+                        <IconMap />
+                        <Link
+                            href="https://www.google.fr/maps/place/Fos-sur-Mer/@43.4545215,4.8155204,12z/data=!4m15!1m8!3m7!1s0x12b619721d5cf72d:0xea40197d819691d!2sFos-sur-Mer!3b1!8m2!3d43.437882!4d4.945711!16zL20vMDkxX3Zi!3m5!1s0x12b619721d5cf72d:0xea40197d819691d!8m2!3d43.437882!4d4.945711!16zL20vMDkxX3Zi!5m1!1e8?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
+                            className={
+                                styles.contact__presentation__text__details__link
+                            }
+                        >
+                            Fos-sur-Mer (13)
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 

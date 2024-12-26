88aaa98ddcdd90e42af7b3a8c962c76b0e7bc8f6
Antoine V <antoine.verove@gmail.com>
2024-12-26 17:22:55 +0100

    Ajoute la gestion du nombre de projet avec la pagination

--- components/ui/molecules/Card/Card.tsx
+++ components/ui/molecules/Card/Card.tsx
@@ -1,14 +1,13 @@
 import Photo from '../../atoms/Photo/Photo'
 import TitleCard from '../../atoms/TitleCard.tsx/TitleCard'
-import voiture from '@/public/voiture.jpg'
 import styles from './card.module.scss'
 import Link from 'next/link'
 import { CardProps } from '@/types/index.js'
 
-const Card = ({ title, description, photo }: CardProps) => {
+const Card = ({ title, description, mainPhoto }: CardProps) => {
     return (
         <div className={styles.card}>
-            <Photo photo={photo} />
+            <Photo photo={mainPhoto} />
             <TitleCard text={title} />
             <div className={styles.card__line}></div>
             <div className={styles.card__summary}>
--- components/ui/organisms/ProjetSection/ProjetSection.tsx
+++ components/ui/organisms/ProjetSection/ProjetSection.tsx
@@ -1,14 +1,32 @@
+'use client'
+
 import Subtitle from '../../atoms/Subtitle/Subtitle'
 import Title from '../../atoms/Title/Title'
 import Card from '../../molecules/Card/Card'
 import styles from './projet-section.module.scss'
-import Button from '@/components/ui/atoms/ButtonArrowRight/ButtonArrowRight'
+import ButtonArrowRight from '@/components/ui/atoms/ButtonArrowRight/ButtonArrowRight'
 import data from '@/data/data.json'
+import { useState } from 'react'
 
 export default function ProjetSection() {
     // définir une constant qui dépend du nombre de projets
     // const projectsCount = nombre d'élément dans le tableau de données
+    const projectsPerPage: number = 6
     const projectsCount: number = data.projects.length
+    const PagesCount = Math.ceil(projectsCount / projectsPerPage)
+
+    const [isExpandedView, setIsExpandedView] = useState<boolean>(false)
+    const [currentPage, setCurrentPage] = useState<number>(1)
+
+    function clickOnAllProjects() {
+        setIsExpandedView(!isExpandedView)
+    }
+    function previousPage() {
+        setCurrentPage(((currentPage - 2 + PagesCount) % PagesCount) + 1)
+    }
+    function nextPage() {
+        setCurrentPage((currentPage % PagesCount) + 1)
+    }
 
     return (
         <section className={styles.projetSection}>
@@ -22,29 +40,75 @@ export default function ProjetSection() {
                         <Title text="PROJETS" />
                         <Subtitle text="VOYAGEZ A TRAVERS MES PROJETS" />
                     </div>
-                    {projectsCount > 3 && <Button text="Tous les projets" />}
+                    {projectsCount > 3 && (
+                        <div onClick={clickOnAllProjects}>
+                            <ButtonArrowRight text="Tous les projets" />
+                        </div>
+                    )}
                 </div>
                 <div className={styles.projetSection__largeScreen__cards}>
                     {/* Afficher le nombre de projets entre 1 et 3 */}
                     {/* Ajouter un état qui prends en compte si l'utilisateur à cliquer sur 
                     'voir tous les projets', Si c'est le cas afficher le nombre de projet max 6 */}
-                    <Card
-                        title="titre"
-                        description="description"
-                        photo={{ src: '/voiture.jpg', alt: 'jjjj' }}
-                    />
-                    <Card
-                        title="titre"
-                        description="description"
-                        photo={{ src: '/voiture.jpg', alt: 'jjjj' }}
-                    />{' '}
-                    <Card
-                        title="titre"
-                        description="description"
-                        photo={{ src: '/voiture.jpg', alt: 'jjjj' }}
-                    />
+                    {isExpandedView
+                        ? data.projects
+                              .slice(
+                                  (currentPage - 1) * projectsPerPage,
+                                  currentPage * projectsPerPage
+                              )
+                              .map((project, i): any => (
+                                  <Card
+                                      key={`project${i}`}
+                                      title={project.title}
+                                      description={project.description}
+                                      mainPhoto={project.mainPhoto}
+                                  />
+                              ))
+                        : data.projects
+                              .slice(
+                                  (currentPage - 1) * projectsPerPage,
+                                  (currentPage - 1) * projectsPerPage + 3
+                              )
+                              .map((project, i): any => (
+                                  <Card
+                                      key={`project${i}`}
+                                      title={project.title}
+                                      description={project.description}
+                                      mainPhoto={project.mainPhoto}
+                                  />
+                              ))}
                 </div>
-                {/* Si projetsCount > 6 alors afficher le sélectionneur de page */}
+                {isExpandedView && (
+                    <div
+                        className={
+                            styles.projetSection__largeScreen__pagination
+                        }
+                    >
+                        <div
+                            className={
+                                styles.projetSection__largeScreen__pagination__previous
+                            }
+                            onClick={previousPage}
+                        >
+                            <ButtonArrowRight />
+                        </div>
+                        <p
+                            className={
+                                styles.projetSection__largeScreen__pagination__p
+                            }
+                        >
+                            {currentPage}
+                        </p>
+                        <div
+                            className={
+                                styles.projetSection__largeScreen__pagination__next
+                            }
+                            onClick={nextPage}
+                        >
+                            <ButtonArrowRight />
+                        </div>
+                    </div>
+                )}
             </div>
         </section>
     )

c4f1dfb10a1837c27411c868c1bb2128937f3e43
Antoine V <antoine.verove@gmail.com>
2024-12-26 12:15:00 +0100

    Ajoute les props à Cards, test avec des propriétés sous forme de texte ok

--- components/ui/atoms/IconMap/IconMap.tsx
+++ components/ui/atoms/IconMap/IconMap.tsx
@@ -7,7 +7,7 @@ const IconMap = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="1"
+            strokeWidth="1"
             strokeLinecap="round"
             strokeLinejoin="round"
             className="feather feather-map-pin"

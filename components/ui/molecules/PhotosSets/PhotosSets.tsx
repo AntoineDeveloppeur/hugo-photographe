9fea9bfd43362a397412f8e16f68e243d28da21f
Antoine V <antoine.verove@gmail.com>
2024-12-28 11:43:59 +0100

    Composant PhotosSets fonctionnel et typé

--- components/ui/molecules/PhotosSets/PhotosSets.tsx
+++ components/ui/molecules/PhotosSets/PhotosSets.tsx
@@ -1,28 +1,60 @@
-
-import styles from './photoSets.module.scss'
-import Photo from '../../atoms/Photo/Photo'
+import Photo from '@/components/ui/atoms/Photo/Photo'
 import { PhotosSetsProps } from '@/types'
-export default function PhotosSets({photosSets} : PhotosSetsProps) {
+import styles from './photosSets.module.scss'
+
+export default function PhotosSets({ photosSets }: PhotosSetsProps) {
     return (
-        {photosSets.map((photosSet, index) => (
-            <div
-                className={styles.projectPage__largeScreen__photosSet}
-                key={`photosSet${index}`}
-            >
-                {photosSet.map((photo, index) => (
-                    <div
-                        style={{
-                            width: `calc(${90 / photosSet.length}%`,
-                        }}
-                        className={
-                            styles.projectPage__largeScreen__photosSet__photoWrapper
-                        }
-                        key={`photoSet${index}`}
-                    >
-                        <Photo photo={photo} />
-                    </div>
-                ))}
-            </div>
-        ))}
+        <>
+            {photosSets.map((photosSet, setIndex) => (
+                <div
+                    className={styles.projectPage__largeScreen__photosSet}
+                    key={`photosSet-${setIndex}`}
+                >
+                    {photosSet.map((photo, photoIndex) => (
+                        <div
+                            style={{
+                                width: `calc(${90 / photosSet.length}%)`,
+                            }}
+                            className={
+                                styles.projectPage__largeScreen__photosSet__photoWrapper
+                            }
+                            key={`photo-${setIndex}-${photoIndex}`}
+                        >
+                            <Photo photo={photo} />
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </>
     )
 }
+
+// import styles from './photosSets.module.scss'
+// import Photo from '../../atoms/Photo/Photo'
+// import { PhotosSetsProps } from '@/types'
+// export default function PhotosSets({ photosSets }: PhotosSetsProps) {
+//     return (
+//         <>
+//             {photosSets.map((photosSet, setIndex) => (
+//                 <div
+//                     className={styles.projectPage__largeScreen__photosSet}
+//                     key={`photosSet${setIndex}`}
+//                 >
+//                     {photosSet.map((photo, photoIndex) => (
+//                         <div
+//                             style={{
+//                                 width: `calc(${90 / photosSet.length}%`,
+//                             }}
+//                             className={
+//                                 styles.projectPage__largeScreen__photosSet__photoWrapper
+//                             }
+//                             key={`photoSet${photoIndex}`}
+//                         >
+//                             <Photo photo={photo} />
+//                         </div>
+//                     ))}
+//                 </div>
+//             ))}
+//         </>
+//     )
+// }

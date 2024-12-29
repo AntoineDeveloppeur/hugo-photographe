4db0248fd5705fdcbfc244f6fb4d13f10836ca93
Antoine V <antoine.verove@gmail.com>
2024-12-29 09:42:35 +0100

    Crée le footer, en cours de construction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.scss'
 import Header from '@/components/ui/organisms/Header/Header'
 import { lora, baskervville, merriweather } from './font'
-
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-})
+import Footer from '@/components/ui/organisms/Footer/Footer'
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -31,6 +21,7 @@ export default function RootLayout({
             >
                 <Header />
                 {children}
+                <Footer />
             </body>
         </html>
     )

fe55cdc664c9bed503c746471bb8a2f866ceea3e
Antoine V <antoine.verove@gmail.com>
2025-01-10 18:50:30 +0100

    Ajoute animation incomplète sur les cards

--- components/ui/molecules/Card/Card.tsx
+++ components/ui/molecules/Card/Card.tsx
@@ -1,14 +1,20 @@
+import styles from './card.module.scss'
 import PhotoBasic from '../../atoms/PhotoBasic/PhotoBasic'
 import TitleCard from '../../atoms/TitleCard/TitleCard'
-import styles from './card.module.scss'
 import Link from 'next/link'
 import { CardProps } from '@/types/index.js'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 const Card = ({ id, title, description, mainPhoto }: CardProps) => {
     const descriptionShort = description.split(' ').slice(0, 20).join(' ')
+    const [isHovered, setIsHovered] = useState(false)
 
     return (
+        <motion.div className={isHovered ? styles.cardWrapper__hovered : styles.cardWrapper}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}>
+
         <Link className={styles.card} href={`/projectPage/${id}`}>
             <PhotoBasic photo={mainPhoto} />
         
@@ -21,7 +27,10 @@ const Card = ({ id, title, description, mainPhoto }: CardProps) => {
                 <p className={styles.card__summary__dots}>... </p>
                 <p className={styles.card__summary__seeMore}>voir plus</p>
             </div>
+            <div className={ isHovered ? styles.hindHover__visible : styles.hindHover}>
+            </div>
         </Link>
+        </motion.div>
     )
 }
 

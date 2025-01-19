d36fbba7027dc476492044007d57b3819dd877ea
Antoine V <antoine.verove@gmail.com>
2025-01-19 11:46:29 +0100

    Import le code du menu choisi, crée la page testMenuMobile pour afficher le menu

---
+++ app/testMenuMobile/page.tsx
@@ -0,0 +1,9 @@
+import MenuMobile from "@/components/ui/molecules/MenuMobile/MenuMobile";
+
+export default function TestMenuMobile() {
+    return (
+        <div>
+            <MenuMobile />
+        </div>
+    );
+}

6f45db682a1997f312b40da6b3a7bd805f98f11b
Antoine V <antoine.verove@gmail.com>
2025-01-21 10:32:01 +0100

    Supprimer la page TestMobile

--- app/testMenuMobile/page.tsx
+++
@@ -1,9 +0,0 @@
-import MenuMobile from "@/components/ui/molecules/MenuMobile/MenuMobile";
-
-export default function TestMenuMobile() {
-    return (
-        <div>
-            <MenuMobile />
-        </div>
-    );
-}

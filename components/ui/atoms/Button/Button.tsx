e7c9ecf2b86b8491f1ed65fb9a3044047c048ff9
Antoine V <antoine.verove@gmail.com>
2025-01-24 18:58:55 +0100

    Ajoute le boutton pour changer de thème

--- components/ui/atoms/Button/Button.tsx
+++ components/ui/atoms/Button/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({ text, link }: ButtonProps) => {
     return (
         <motion.button
             className={styles.button}
-            whileHover={{ backgroundColor: 'var(--dark7)' }}
+            whileHover={{ backgroundColor: 'var(--background-button)' }}
             whileTap={{ scale: 0.9 }}
         >
             {link ? (

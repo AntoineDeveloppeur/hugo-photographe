b09f07c6b098c6752b4004223d045c7c62774c2b
Antoine V <antoine.verove@gmail.com>
2025-02-06 18:12:26 +0100

    Ajoute la fermeture du menu lorsque l'utilisateur clique en dehours

--- components/ui/molecules/ThemeChanger/ThemeChanger.tsx
+++ components/ui/molecules/ThemeChanger/ThemeChanger.tsx
@@ -5,15 +5,12 @@ import styles from './theme-changer.module.scss'
 import { useTheme } from 'next-themes'
 import { useState, useEffect } from 'react'
 
-type Theme = "light" | "dark"
-
 export default function ThemeChanger() {
     const [mounted, setMounted] = useState(false)
     const {theme, setTheme, systemTheme} = useTheme()
 
     const toggleSwitch = () => {
-        setTheme(() => (currentTheme === "dark" ? "light" : "dark"))
-        console.log(theme)
+        
     }
 
     
@@ -26,7 +23,7 @@ export default function ThemeChanger() {
     const currentTheme = theme === "system" ? systemTheme : theme
 
     return (
-        <div className={styles.themeChanger} onClick={toggleSwitch}>
+        <div className={styles.themeChanger} onClick={() =>setTheme(() => (currentTheme === "dark" ? "light" : "dark"))}>
             <ButtonTheme theme={currentTheme} />
             <p className={styles.themeChanger__p}>
                 {currentTheme === "light" ? "Mode sombre" : "Mode clair"}</p>

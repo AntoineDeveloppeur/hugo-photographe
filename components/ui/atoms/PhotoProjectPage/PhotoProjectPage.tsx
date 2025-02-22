c2bc90a5b2eaa31094b4d24cd7e8a99c75b2fb46
Antoine V <antoine.verove@gmail.com>
2025-02-22 11:52:23 +0100

    Améliore la gestion de la taille des images mais reste à gérer les formats portraits et paysage

--- components/ui/atoms/PhotoProjectPage/PhotoProjectPage.tsx
+++ components/ui/atoms/PhotoProjectPage/PhotoProjectPage.tsx
@@ -39,7 +39,7 @@ const PhotoGallery = ({ photo, hoverEffect }: PhotoProps) => {
                     duration: 5, 
                     ease: [0.215, 0.61, 0.355, 1]
                 }}
-                style={{ height: '100%', width: 'auto'}}
+                style={{ height: 'auto', width: '100%'}}
             >
                 <div className={styles.imageWrapper}>
                     <NextImage

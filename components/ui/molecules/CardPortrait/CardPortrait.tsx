e0737f82f16ca2616dc01ca1d0349f0e47f3a8d7
Antoine V <antoine.verove@gmail.com>
2025-03-04 16:42:12 +0100

    Ajoute un fond au cartes même si pas hover

--- components/ui/molecules/CardPortrait/CardPortrait.tsx
+++ components/ui/molecules/CardPortrait/CardPortrait.tsx
@@ -11,21 +11,20 @@ const Card = ({ id, title, summary, mainPhoto }: CardProps) => {
         <div
         className={styles.cardWrapper}
         >
-
-        <Link className={styles.cardWrapper__card} href={`/projectPage/${id}`}>
-            <div className={styles.cardWrapper__gradientLayer}></div>
-                <PhotoBasic photo={mainPhoto} />
-        
-            <div className={styles.cardWrapper__card__title}>
-                <TitleCard text={title} />
-            </div>
-            <div className={styles.cardWrapper__card__line}></div>
-            <div className={styles.cardWrapper__card__summary}>
-                <p className={styles.cardWrapper__card__summary__text}>{summary}...&nbsp;
-                <span className={styles.cardWrapper__card__summary__seeMore}>voir plus</span>
-                </p>
-            </div>
-        </Link>
+            <Link className={styles.cardWrapper__card} href={`/projectPage/${id}`}>
+                <div className={styles.cardWrapper__gradientLayer}></div>
+                    <PhotoBasic photo={mainPhoto} />
+            
+                <div className={styles.cardWrapper__card__title}>
+                    <TitleCard text={title} />
+                </div>
+                <div className={styles.cardWrapper__card__line}></div>
+                <div className={styles.cardWrapper__card__summary}>
+                    <p className={styles.cardWrapper__card__summary__text}>{summary}...&nbsp;
+                    <span className={styles.cardWrapper__card__summary__seeMore}>voir plus</span>
+                    </p>
+                </div>
+            </Link>
         </div>
     )
 }

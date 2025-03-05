1b8011a13786e0d9af17f8115a230869da646131
Antoine V <antoine.verove@gmail.com>
2025-03-05 15:36:43 +0100

    Medal dans le composant CardLandcape et répond au changement de thème

--- components/ui/molecules/CardPortrait/CardPortrait.tsx
+++ components/ui/molecules/CardPortrait/CardPortrait.tsx
@@ -5,33 +5,10 @@ import PhotoBasic from '../../atoms/PhotoBasic/PhotoBasic'
 import TitleCard from '../../atoms/TitleCard/TitleCard'
 import Link from 'next/link'
 import { CardProps } from '@/types/index.js'
-import { useState, useRef } from 'react'
-import { Player } from '@lordicon/react'
 import ICON from '@/public/medal.json'
-import { useScroll, useMotionValueEvent } from 'framer-motion'
 
 export default function CardPortrait ({ id, title, summary, mainPhoto }: CardProps) {
-    const playerRef = useRef<Player>(null)
-    const playerRefPosition = useRef<HTMLDivElement>(null)
-    const [scrollProgress, setScrollProgress] = useState(0)
-    const [isAnimationStarted, setIsAnimationStarted] = useState(false)
-    
-    const { scrollYProgress } = useScroll({
-        target: playerRefPosition,
-        offset: ["start end", "end start"]
-    })
-    
-    // Utiliser useMotionValueEvent pour suivre les changements de scrollYProgress
-    useMotionValueEvent(scrollYProgress, "change", (latest) => {
-        setScrollProgress(latest)
-        
-        if (latest > 0.1 && !isAnimationStarted) {
-            playerRef.current?.playFromBeginning()
-            setIsAnimationStarted(true)
-        } else if (latest <= 0.1 && isAnimationStarted) {
-            setIsAnimationStarted(false)
-        }
-    })
+
 
     return (
         <div
@@ -43,11 +20,9 @@ export default function CardPortrait ({ id, title, summary, mainPhoto }: CardPro
                 <PhotoBasic photo={mainPhoto} />
         
             <div 
-                ref={playerRefPosition} 
                 className={styles.cardWrapper__card__title}
                 >
                 <TitleCard text={title} />
-                <Player ref={playerRef} icon={ICON} />
             </div>
             <div className={styles.cardWrapper__card__line}></div>
             <div className={styles.cardWrapper__card__summary}>

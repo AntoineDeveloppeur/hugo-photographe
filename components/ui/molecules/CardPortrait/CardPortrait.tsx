dd434c12acfd669a1234715ef6d7aeec809dd9c8
Antoine V <antoine.verove@gmail.com>
2025-03-05 12:03:28 +0100

    La médaille est bien animée lors du scroll

--- components/ui/molecules/CardPortrait/CardPortrait.tsx
+++ components/ui/molecules/CardPortrait/CardPortrait.tsx
@@ -5,16 +5,33 @@ import PhotoBasic from '../../atoms/PhotoBasic/PhotoBasic'
 import TitleCard from '../../atoms/TitleCard/TitleCard'
 import Link from 'next/link'
 import { CardProps } from '@/types/index.js'
-import { useEffect, useRef} from 'react'
+import { useState, useRef } from 'react'
 import { Player } from '@lordicon/react'
 import ICON from '@/public/medal.json'
+import { useScroll, useMotionValueEvent } from 'framer-motion'
 
 export default function CardPortrait ({ id, title, summary, mainPhoto }: CardProps) {
     const playerRef = useRef<Player>(null)
-
-    useEffect(() => {
-        playerRef.current?.playFromBeginning();
-    }, [])
+    const playerRefPosition = useRef<HTMLDivElement>(null)
+    const [scrollProgress, setScrollProgress] = useState(0)
+    const [isAnimationStarted, setIsAnimationStarted] = useState(false)
+    
+    const { scrollYProgress } = useScroll({
+        target: playerRefPosition,
+        offset: ["start end", "end start"]
+    })
+    
+    // Utiliser useMotionValueEvent pour suivre les changements de scrollYProgress
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        setScrollProgress(latest)
+        
+        if (latest > 0.1 && !isAnimationStarted) {
+            playerRef.current?.playFromBeginning()
+            setIsAnimationStarted(true)
+        } else if (latest <= 0.1 && isAnimationStarted) {
+            setIsAnimationStarted(false)
+        }
+    })
 
     return (
         <div
@@ -25,7 +42,10 @@ export default function CardPortrait ({ id, title, summary, mainPhoto }: CardPro
             <div className={styles.cardWrapper__gradientLayer}></div>
                 <PhotoBasic photo={mainPhoto} />
         
-            <div className={styles.cardWrapper__card__title}>
+            <div 
+                ref={playerRefPosition} 
+                className={styles.cardWrapper__card__title}
+                >
                 <TitleCard text={title} />
                 <Player ref={playerRef} icon={ICON} />
             </div>

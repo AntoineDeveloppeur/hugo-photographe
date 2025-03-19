aac6c0f1bc9deb24a8e6203ac19070283ee24ddf
Antoine V <antoine.verove@gmail.com>
2025-03-19 10:10:17 +0100

    Ajoute les balises openGraph et Twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,35 @@ import { lora, baskervville, merriweather } from './font'
 import { Metadata } from 'next'
 import Providers from '@/components/Providers'
 
+// Récupérer l'URL de base à partir d'une variable d'environnement ou utiliser l'URL de base par défaut
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://photographe-hugo-randez.fr'
+
 export const metadata: Metadata = {
-    title: 'Hugo Photographe',
-    description: 'Portfolio de Hugo Randez, photographe professionnel',
+    title: 'Photographe du monde Hugo Randez',
+    description: 'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
+    openGraph: {
+        title: 'Photographe du monde Hugo Randez',
+        description: 'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
+        url: baseUrl,
+        siteName: 'Photographe du monde Hugo Randez',
+        images: [
+            {
+                url: `${baseUrl}/images/og-image.jpg`,
+                width: 1200,
+                height: 630,
+                alt: 'Hugo Randez Photographe',
+            },
+        ],
+        locale: 'fr_FR',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Photographe du monde Hugo Randez',
+        description: 'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
+        // creator: '@hugorandez',
+        images: [`${baseUrl}/images/twitter-image.jpg`],
+    },
 }
 
 export default function RootLayout({

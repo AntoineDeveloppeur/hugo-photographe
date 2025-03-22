a11f79bce0885cc39e81f74a47bbfefd989a17fd
Antoine V <antoine.verove@gmail.com>
2025-03-22 19:38:32 +0100

    Serveur fonctionnel avec une requête get

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,37 @@
-import express from 'express'
-import node from 'node'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Configuration des variables d'environnement
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '.env') });
 
-const app = express.create()
+// Crée l'application 
+const app = express();
 
-app.listen()
+const PORT = process.env.SERVER_PORT || 3002;
+
+// Middleware
+app.use(express.json());
+    // Permet de traiter les formulaire, l'option extended à true autorise l'analyse d'objets complexes et imbriqués
+app.use(express.urlencoded({ extended: true }));
+
+// Configuration CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Remplacer par l'URL de votre frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'API de Hugo Photographe fonctionne correctement' });
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});

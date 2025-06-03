11310e5b4472c2c1eb1870a2a3e708f3779684f1
Antoine V <antoine.verove@gmail.com>
2025-06-03 16:53:55 +0200

    Ajoute la page administrateur

---
+++ app/administrateur/page.tsx
@@ -0,0 +1,18 @@
+import TitleProjectPage from '@/components/ui/atoms/TitleProjectPage/TitleProjectPage'
+import styles from './administrateur.module.scss'
+import Button from '@/components/ui/atoms/Button/Button'
+
+export default function Administrateur() {
+    return (
+        <section className={styles.administrateur}>
+            <div className={styles.administrateur__largeScreen}>
+                <div className={styles.administrateur__largeScreen__titleWrapper}>
+                    <TitleProjectPage text="Administrateur" />
+                </div>
+                <Button text="Ajouter un Projet" link={'/ajouterProjet'} />
+                <Button text="Supprimer un Projet" link={'/supprimerProjet'}/>
+
+            </div>
+        </section>
+    )
+}

96493f8e35bdeeee0d30b71c6fb97206ecd1a048
Antoine V <antoine.verove@gmail.com>
2025-06-12 14:31:30 +0200

    Revalidate data from getProjects every 20 seconds

--- utils/getProjects.ts
+++ utils/getProjects.ts
@@ -5,7 +5,7 @@ export default async function getProjects(): Promise<Data> {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/api/project/getProjects`,
-      { next: { revalidate: 3600 } }
+      { next: { revalidate: 20 } }
     )
 
     if (!response.ok) {

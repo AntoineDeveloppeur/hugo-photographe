2117c9da692192926d4b786ef139aef561f55519
Antoine V <antoine.verove@gmail.com>
2025-06-17 15:23:06 +0200

    Skeleton en cours d'ajout mais non fonctionnel

--- components/ui/atoms/PhotoProjectPage/PhotoProjectPage.tsx
+++ components/ui/atoms/PhotoProjectPage/PhotoProjectPage.tsx
@@ -4,72 +4,85 @@ import styles from './photo-project-page.module.scss'
 import { PhotoProps } from '@/types'
 import NextImage from 'next/image'
 import { motion } from 'framer-motion'
-import { useState } from 'react'
+import { useState, Suspense } from 'react'
 import Modal from '../Modal/Modal'
 import Loader from '../Loader/Loader'
 import useIsMobile from '@/hooks/useIsMobile'
 import ServerImage from '../ServerImage/ServerImage'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-const PhotoProjectPage = ({ photo, hoverEffect, priority, sizes, mainPhoto }: PhotoProps) => {
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    const [isLoading, setIsLoading] = useState(true)
-    const [isHovered, setIsHovered] = useState(false)
-    const isMobile = useIsMobile()
+const PhotoProjectPage = ({
+  photo,
+  hoverEffect,
+  priority,
+  sizes,
+  mainPhoto,
+}: PhotoProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isHovered, setIsHovered] = useState(false)
+  const isMobile = useIsMobile()
 
-    const handleModalClose = () => {
-        setIsModalOpen(false)
-    }
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+  }
 
-    const handleMouseEnter = () => {
-        setIsLoading(true)
-        setIsHovered(true)
-    }
+  const handleMouseEnter = () => {
+    setIsLoading(true)
+    setIsHovered(true)
+  }
 
-    return (
-        <>
-            <motion.div 
-                onClick={() => !isMobile && setIsModalOpen(true)}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={() => setIsHovered(false)}
-                whileHover={hoverEffect ? { scale: 1.08 } : undefined}
-                whileTap={hoverEffect ? { scale: 1.08 } : undefined}
-                transition={{ 
-                    duration: 5, 
-                    ease: [0.215, 0.61, 0.355, 1]
-                }}
-                style={!isMobile ? {'cursor': 'pointer'} : undefined}
-            >
-                <ServerImage 
-                    photo={photo}
-                    priority={priority}
-                    sizes={sizes}
-                    mainPhoto={mainPhoto}
-                    className={styles.imageWrapper}
-                    imageClassName={styles.imageWrapper__image}
-                />
-            </motion.div>
+  return (
+    <>
+      <motion.div
+        onClick={() => !isMobile && setIsModalOpen(true)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={() => setIsHovered(false)}
+        whileHover={hoverEffect ? { scale: 1.08 } : undefined}
+        whileTap={hoverEffect ? { scale: 1.08 } : undefined}
+        transition={{
+          duration: 5,
+          ease: [0.215, 0.61, 0.355, 1],
+        }}
+        style={!isMobile ? { cursor: 'pointer' } : undefined}
+      >
+        <Suspense
+          fallback={
+            <Skeleton height="100%" width="100%" className={styles.skeleton} />
+          }
+        >
+          <ServerImage
+            photo={photo}
+            priority={priority}
+            sizes={sizes}
+            mainPhoto={mainPhoto}
+            className={styles.imageWrapper}
+            imageClassName={styles.imageWrapper__image}
+          />
+        </Suspense>
+      </motion.div>
 
-            <Modal
-                isOpen={isModalOpen}
-                onClose={handleModalClose}
-            >
-                <div className={styles.modalImageContainer}>
-                    {isLoading && <Loader />}
-                    <NextImage
-                        className={`${styles.modalImage} ${!isLoading ? styles.loaded : ''}`}
-                        src={photo.src}
-                        alt={photo.alt}
-                        width={photo.width}
-                        height={photo.height}
-                        quality={100}
-                        priority={isHovered || isModalOpen}
-                        onLoadingComplete={() => setIsLoading(false)}
-                        style={{ objectFit: 'contain' }}
-                    />
-                </div>
-            </Modal>
-        </>
-    )
+      <Modal isOpen={isModalOpen} onClose={handleModalClose}>
+        <div className={styles.modalImageContainer}>
+          {isLoading && <Loader />}
+          <NextImage
+            className={`${styles.modalImage} ${
+              !isLoading ? styles.loaded : ''
+            }`}
+            src={photo.src}
+            alt={photo.alt}
+            width={photo.width}
+            height={photo.height}
+            quality={100}
+            priority={isHovered || isModalOpen}
+            onLoadingComplete={() => setIsLoading(false)}
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+      </Modal>
+    </>
+  )
 }
 
 export default PhotoProjectPage
--- components/ui/atoms/ServerImage/ServerImage.tsx
+++ components/ui/atoms/ServerImage/ServerImage.tsx
@@ -3,35 +3,35 @@ import { PhotoProps } from '@/types'
 import styles from './serverImage.module.scss'
 
 type ServerImageProps = {
-  photo: PhotoProps['photo'];
-  priority?: boolean;
-  sizes?: string;
-  mainPhoto?: boolean;
-  className?: string;
-  imageClassName?: string;
-  style?: React.CSSProperties;
-  quality?: number;
+  photo: PhotoProps['photo']
+  priority?: boolean
+  sizes?: string
+  mainPhoto?: boolean
+  className?: string
+  imageClassName?: string
+  style?: React.CSSProperties
+  quality?: number
 }
 
-export default function ServerImage({ 
-  photo, 
-  priority = false, 
-  sizes = "(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw", 
+export default function ServerImage({
+  photo,
+  priority = false,
+  sizes = '(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw',
   mainPhoto = false,
   className,
   imageClassName,
   style,
-  quality = 75
+  quality = 75,
 }: ServerImageProps) {
   // Calcul du style conditionnel pour les photos principales (mainPhoto)
-  const mainPhotoStyle = mainPhoto 
-    ? (photo.height > photo.width 
-        ? {'height': '65vh', 'width': 'auto'} 
-        : {'height': 'auto', 'width': '100vw'}) 
-    : undefined;
+  const mainPhotoStyle = mainPhoto
+    ? photo.height > photo.width
+      ? { height: '65vh', width: 'auto' }
+      : { height: 'auto', width: '100vw' }
+    : undefined
 
   // Combinaison des styles personnalisés et des styles mainPhoto
-  const combinedStyle = mainPhoto ? {...mainPhotoStyle, ...style} : style;
+  const combinedStyle = mainPhoto ? { ...mainPhotoStyle, ...style } : style
 
   return (
     <div className={`${styles.imageWrapper} ${className || ''}`}>

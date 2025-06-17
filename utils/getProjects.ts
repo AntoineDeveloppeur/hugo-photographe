def8a2a7ed9974a04c50f8ed72da2fb3a81233b1
Antoine V <antoine.verove@gmail.com>
2025-06-17 14:02:39 +0200

    Test revalidate fini mais pas concluant, à reprendre plus tard

--- utils/getProjects.ts
+++ utils/getProjects.ts
@@ -5,7 +5,7 @@ export default async function getProjects(): Promise<Data> {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/api/project/getProjects`,
-      { next: { revalidate: false } }
+      { next: { revalidate: 3600 } }
       // { cache: 'force-cache', next: { revalidate: false } }
     )
 

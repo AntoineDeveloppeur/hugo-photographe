3424148c1859a00b06c7b2ba6bc893dd3d48cd28
Antoine V <antoine.verove@gmail.com>
2025-06-23 17:12:53 +0200

    Changement architecture public

--- components/ui/molecules/CardLandscape/CardLandscape.tsx
+++ components/ui/molecules/CardLandscape/CardLandscape.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import styles from './card-landscape.module.scss'
 import PhotoBasic from '../../atoms/PhotoBasic/PhotoBasic'
@@ -7,82 +7,108 @@ import Link from 'next/link'
 import { CardProps } from '@/types/index.js'
 import { Player } from '@lordicon/react'
 import { useMotionValueEvent, useScroll } from 'framer-motion'
-import Medal from '@/public/medal.json'
+import Medal from '@/public/icons/medal.json'
 import { useState, useRef } from 'react'
 import { useTheme } from 'next-themes'
 import IconDelete from '../../atoms/IconDelete/IconDelete'
 import ModalDeleteProject from '../ModalDeleteProject/ModalDeleteProject'
 
+export default function CardLandscape({
+  _id,
+  title,
+  summary,
+  mainPhoto,
+  deleteIcon,
+}: CardProps) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const handleDelete = () => {
+    setIsModalOpen(true)
+  }
 
-export default function CardLandscape({ _id, title, summary, mainPhoto, deleteIcon }: CardProps) {
+  const playerRef = useRef<Player>(null)
+  const medalWrapper = useRef<HTMLDivElement>(null)
+  const [isAnimationStarted, setIsAnimationStarted] = useState<boolean>(false)
 
-    
-    const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false)
-    const handleDelete = () => {
-        setIsModalOpen(true)
+  // Utiliser useTheme pour forcer un rendu quand le thème change
+  const { theme } = useTheme()
+
+  // Utilise useScroll pour déclencher l'animation lorsque l'utilisateur se rapproche de la médaille
+  const { scrollYProgress } = useScroll({
+    target: medalWrapper,
+    offset: ['start end', 'end start'],
+  })
+
+  // Surveiller le changement de valeur avec useMotionValueEvent
+  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+    if (latest > 0.2 && !isAnimationStarted) {
+      playerRef.current?.playFromBeginning()
+      setIsAnimationStarted(true)
     }
+    if (latest <= 0.2 && isAnimationStarted) {
+      setIsAnimationStarted(false)
+    }
+  })
 
-
-    const playerRef = useRef<Player>(null)
-    const medalWrapper = useRef<HTMLDivElement>(null)
-    const [isAnimationStarted, setIsAnimationStarted] = useState<boolean>(false)
-    
-    // Utiliser useTheme pour forcer un rendu quand le thème change
-    const { theme } = useTheme()
-    
-    // Utilise useScroll pour déclencher l'animation lorsque l'utilisateur se rapproche de la médaille
-    const {scrollYProgress} = useScroll({
-        target: medalWrapper,
-        offset: ["start end", "end start"]
-    });
-
-    // Surveiller le changement de valeur avec useMotionValueEvent
-    useMotionValueEvent(scrollYProgress, 'change', (latest) => {
-        if (latest > 0.2 && !isAnimationStarted) {
-            playerRef.current?.playFromBeginning()
-            setIsAnimationStarted(true)
-        }
-        if (latest <= 0.2 && isAnimationStarted) {
-            setIsAnimationStarted(false)
-        }
-    });
-
-    return (
-        <div className={styles.cardWrapper}>
-            {deleteIcon && 
-            <div className={styles.cardWrapper__deleteWrapper} onClick={handleDelete}>
-                <IconDelete/>
-            </div>
-            }
-            {isModalOpen &&
-            <ModalDeleteProject _id={_id} title={title} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}  />
-            }
-            <Link className={styles.cardWrapper__card} href={`/projectPage/${_id}`}>
-                <div ref={medalWrapper} className={styles.cardWrapper__card__medalWrapper}>
-                    <Player 
-                        ref={playerRef} 
-                        icon={Medal} 
-                        size={64} 
-                        state='in-reveal' 
-                        colors={theme === 'light' ? 'primary:#D96E75,secondary:#f0cace' : 'primary:#e5e5e5,secondary:#919191'}
-                    />
-                </div>
-                <div className={styles.cardWrapper__card__gradientLayer}></div>
-                <div className={styles.cardWrapper__card__photoWrapper}>
-                    <PhotoBasic photo={mainPhoto} sizes="(max-width: 767px) 100vw, 45vw" />
-                </div>
-                <div className={styles.cardWrapper__card__text}>
-                    <div className={styles.cardWrapper__card__text__title}>
-                        <TitleCard text={title} />
-                    </div>
-                    <div className={styles.cardWrapper__card__text__line}></div>
-                    <div className={styles.cardWrapper__card__text__summary}>
-                        <p className={styles.cardWrapper__card__text__summary__text}>{summary}</p>
-                        <p className={styles.cardWrapper__card__text__summary__dots}>... </p>
-                        <p className={styles.cardWrapper__card__text__summary__seeMore}>voir plus</p>
-                    </div>
-                </div>
-            </Link>
+  return (
+    <div className={styles.cardWrapper}>
+      {deleteIcon && (
+        <div
+          className={styles.cardWrapper__deleteWrapper}
+          onClick={handleDelete}
+        >
+          <IconDelete />
         </div>
-    )
+      )}
+      {isModalOpen && (
+        <ModalDeleteProject
+          _id={_id}
+          title={title}
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+      <Link className={styles.cardWrapper__card} href={`/projectPage/${_id}`}>
+        <div
+          ref={medalWrapper}
+          className={styles.cardWrapper__card__medalWrapper}
+        >
+          <Player
+            ref={playerRef}
+            icon={Medal}
+            size={64}
+            state="in-reveal"
+            colors={
+              theme === 'light'
+                ? 'primary:#D96E75,secondary:#f0cace'
+                : 'primary:#e5e5e5,secondary:#919191'
+            }
+          />
+        </div>
+        <div className={styles.cardWrapper__card__gradientLayer}></div>
+        <div className={styles.cardWrapper__card__photoWrapper}>
+          <PhotoBasic
+            photo={mainPhoto}
+            sizes="(max-width: 767px) 100vw, 45vw"
+          />
+        </div>
+        <div className={styles.cardWrapper__card__text}>
+          <div className={styles.cardWrapper__card__text__title}>
+            <TitleCard text={title} />
+          </div>
+          <div className={styles.cardWrapper__card__text__line}></div>
+          <div className={styles.cardWrapper__card__text__summary}>
+            <p className={styles.cardWrapper__card__text__summary__text}>
+              {summary}
+            </p>
+            <p className={styles.cardWrapper__card__text__summary__dots}>
+              ...{' '}
+            </p>
+            <p className={styles.cardWrapper__card__text__summary__seeMore}>
+              voir plus
+            </p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
 }

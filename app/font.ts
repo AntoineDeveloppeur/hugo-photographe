0f9877a7dd461eecbd2525b0441dc670ba37b0a6
Antoine V <antoine.verove@gmail.com>
2025-06-23 10:38:20 +0200

    Changement police signature

--- app/font.ts
+++ app/font.ts
@@ -1,22 +1,33 @@
 import { Lora, Baskervville, Merriweather } from 'next/font/google'
+import localFont from 'next/font/local'
 
 export const lora = Lora({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-lora',
-    weight: '500',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lora',
+  weight: '500',
 })
 
 export const baskervville = Baskervville({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-baskervville',
-    weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-baskervville',
+  weight: '400',
 })
 
 export const merriweather = Merriweather({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-merriweather',
-    weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-merriweather',
+  weight: '400',
 })
+
+export const taviraj = localFont({
+  src: '../public/fonts/Taviraj-Regular.ttf',
+  variable: '--font-taviraj',
+})
+
+// export const corben = localFont({
+//   src: '../public/fonts/Corben-Regular.ttf',
+//   variable: '--font-corben',
+// })
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,63 @@
 import './globals.scss'
-import { lora, baskervville, merriweather } from './font'
+import { lora, baskervville, merriweather, taviraj } from './font'
 import { Metadata } from 'next'
 import Providers from '@/components/Providers'
 
 // Récupérer l'URL de base à partir d'une variable d'environnement ou utiliser l'URL de base par défaut
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://photographe-hugo-randez.fr'
+const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL || 'https://photographe-hugo-randez.fr'
 
 export const metadata: Metadata = {
+  title: 'Photographe du monde Hugo Randez',
+  description:
+    'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-icon.png',
+  },
+  openGraph: {
     title: 'Photographe du monde Hugo Randez',
-    description: 'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
-    icons: {
-        icon: '/favicon.ico',
-        shortcut: '/favicon.ico',
-        apple: '/apple-icon.png',
-    },
-    openGraph: {
-        title: 'Photographe du monde Hugo Randez',
-        description: 'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
-        url: baseUrl,
-        siteName: 'Photographe du monde Hugo Randez',
-        images: [
-            {
-                url: `${baseUrl}/images/og-image.jpg`,
-                width: 1200,
-                height: 630,
-                alt: 'Hugo Randez Photographe',
-            },
-        ],
-        locale: 'fr_FR',
-        type: 'website',
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Photographe du monde Hugo Randez',
-        description: 'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
-        images: [`${baseUrl}/images/twitter-image.jpg`],
-    },
+    description:
+      'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
+    url: baseUrl,
+    siteName: 'Photographe du monde Hugo Randez',
+    images: [
+      {
+        url: `${baseUrl}/images/og-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: 'Hugo Randez Photographe',
+      },
+    ],
+    locale: 'fr_FR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Photographe du monde Hugo Randez',
+    description:
+      'Découvrez des destinations exotiques et vivez des aventures uniques avec les photos de voyage de Hugo Randez. Une expérience visuelle inoubliable.',
+    images: [`${baseUrl}/images/twitter-image.jpg`],
+  },
 }
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode
+  children: React.ReactNode
 }>) {
-    return (
-        <html lang="fr">
-            <body className={`${lora.variable} ${baskervville.variable} ${merriweather.variable} antialiased`}>
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    )
+  return (
+    <html lang="fr">
+      <body
+        className={`${lora.variable} 
+        ${baskervville.variable} 
+        ${merriweather.variable} 
+        ${taviraj.variable}
+        antialiased`}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
 }

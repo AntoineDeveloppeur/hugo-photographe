d74b72609ef09cdb5a34521b26420c80bf53e677
Antoine Developpeur <antoine.verove@gmail.com>
2025-07-08 16:36:40 +0200

    Ajoute un test à useReCaptcha

--- __tests__/useReCaptcha.test.js
+++ __tests__/useReCaptcha.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act, waitFor } from "@testing-library/react"
+import { renderHook, waitFor } from "@testing-library/react"
 import useReCaptcha from "@/hooks/useReCaptcha"
 
 global.fetch = jest.fn()
@@ -16,18 +16,38 @@ describe("useReCaptcha", () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
+  it("should return success: false if executeRecaptcha is undefined", async () => {
+    // Arrange
+    jest.mock("react-google-recaptcha-v3", () => ({
+      useGoogleReCaptcha: () => ({
+        executeRecaptcha: jest.fn().mockResolvedValueOnce(undefined),
+      }),
+    }))
+
+    // Act
+    const { result } = renderHook(useReCaptcha)
+
+    // Assert
+    await waitFor(() => {
+      expect(result.current.success).toBe(false)
+    })
+  })
   it("should return success: false if fetch is rejected", async () => {
     // Arrange
     global.fetch.mockRejectedValueOnce()
+    jest.mock("react-google-recaptcha-v3", () => ({
+      useGoogleReCaptcha: () => ({
+        executeRecaptcha: jest.fn().mockResolvedValue("mock-token"),
+      }),
+    }))
 
     // Act
     renderHook(() => useReCaptcha())
-    await waitFor(() => {
-      // Assert
-      expect(console.error).toHaveBeenCalled()
-    })
 
     // Assert
+    await waitFor(() => {
+      expect(console.error).toHaveBeenCalled()
+    })
     expect(global.fetch).toHaveBeenCalledWith(
       "https://test-url.com/api/recaptcha",
       {
@@ -38,5 +58,33 @@ describe("useReCaptcha", () => {
         body: JSON.stringify({ token: "mock-token" }),
       }
     )
+  })
+  it("should return valid email, phone and name if fetch succeed", async () => {
+    // Arrange
+    jest.mock("react-google-recaptcha-v3", () => ({
+      useGoogleReCaptcha: () => ({
+        executeRecaptcha: jest.fn().mockResolvedValue("mock-token"),
+      }),
+    }))
+    global.fetch.mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue({
+        success: true,
+        name: "h",
+        email: "fdsq@fdsq.com",
+        phone: "06 00 00 00 00",
+      }),
+    })
+
+    // Act
+    const { result } = renderHook(() => useReCaptcha())
+
+    // Assert
+    await waitFor(() => {
+      expect(result.current.success).toBe(true)
+      expect(typeof result.current.name).toBe("string")
+      expect(result.current.email).toMatch(/@/)
+      expect(result.current.phone).toMatch(/^\d{2}(\s\d{2}){4}$/)
+    })
   })
 })

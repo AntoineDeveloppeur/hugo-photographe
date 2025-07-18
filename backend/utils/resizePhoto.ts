fe16fb28e763a2c65acc39bb1beec109a000623b
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2025-07-18 18:35:12 +0200

    feat: Résolu problème de typage

--- backend/utils/resizePhoto.ts
+++ backend/utils/resizePhoto.ts
@@ -1,4 +1,8 @@
-const calculateResizeDimensions = (width: number, height: number) => {
+import { v4 as uuidv4 } from "uuid" // Pour générer des noms de fichiers uniques
+import path from "path"
+import sharp from "sharp"
+
+export const calculateResizeDimensions = (width: number, height: number) => {
   const maxWidth = 3840
   const maxHeight = 2160
   if (width <= maxWidth && height <= maxHeight) {
@@ -15,4 +19,26 @@ const calculateResizeDimensions = (width: number, height: number) => {
   }
 }
 
-export default calculateResizeDimensions
+type resizePhoto = {
+  metadata: { format?: string | undefined }
+  file: { filepath: string; mimetype: string; originalFilename: string }
+  newDimensions: { width: number; height: number }
+}
+
+export const resizePhoto = async ({
+  metadata,
+  file,
+  newDimensions,
+}: resizePhoto) => {
+  const uniqueId = uuidv4()
+  const resizedFilePath = path.join(
+    path.dirname(file.filepath),
+    // TODO: 'jpg' est pris arbitrairement, cela n'a pas d'incidence sur la suite
+    // A travailler pour rendre le code plus propre
+    `${uniqueId}.${metadata.format || "jpg"}`
+  )
+  await sharp(file.filepath)
+    .resize(newDimensions.width, newDimensions.height)
+    .toFile(resizedFilePath)
+  return { ...file, filepath: resizedFilePath }
+}

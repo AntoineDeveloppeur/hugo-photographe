5ff2cc2e41b0896b6e6fe8a3e9f7cbea7282551c
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2025-09-17 17:54:31 +0200

    fix: Permet les requêtes du serveur frontend au serveur backend

--- utils/getProjects.ts
+++ utils/getProjects.ts
@@ -2,6 +2,7 @@ import dataFallBack from "@/data/data.json"
 import { Data } from "@/types"
 
 export default async function getProjects(): Promise<Data> {
+  console.log("je suis dans getProjects")
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/api/project/getProjects`,
@@ -11,11 +12,34 @@ export default async function getProjects(): Promise<Data> {
     )
 
     if (!response.ok) {
+      console.log("je suis dans getProjects return data")
       return dataFallBack
     }
     const data: Data = await response.json()
     return data
   } catch {
-    return dataFallBack
+    try {
+      console.log("je suis dans getProjects tentative avec SERVER_URL")
+      const response2 = await fetch(
+        `${process.env.SERVER_URL}/api/project/getProjects`,
+        { next: { revalidate: 2 } }
+        // Could use an High revalidate value but a low value improve administrator UX when creating projects
+        //
+        // Moreover, site load is going to be low, there is not risk of too many requests
+      )
+
+      if (!response2.ok) {
+        console.log("je suis dans getProjects !response2.ok")
+        return dataFallBack
+      }
+      console.log("je suis dans getProjects return data2")
+
+      const data2: Data = await response2.json()
+      return data2
+    } catch {
+      console.log("je suis dans getProjects catch")
+
+      return dataFallBack
+    }
   }
 }

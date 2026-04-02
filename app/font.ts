3dbc27ab30bc53bf22c39cff07c352a1f4a9617a
Antoine <159899181+AntoineDeveloppeur@users.noreply.github.com>
2026-04-02 11:24:41 +0200

    feat: télécharge les polices en local pour éviter les requêtes en prod

--- app/font.ts
+++ app/font.ts
@@ -1,33 +1,28 @@
-import { Lora, Baskervville, Merriweather } from 'next/font/google'
-import localFont from 'next/font/local'
+import localFont from "next/font/local"
 
-export const lora = Lora({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-lora',
-  weight: '500',
+export const lora = localFont({
+  src: "../public/fonts/lora-v37-latin-500.woff2",
+  variable: "--font-lora",
+  display: "swap",
+  weight: "500",
 })
 
-export const baskervville = Baskervville({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-baskervville',
-  weight: '400',
+export const baskervville = localFont({
+  src: "../public/fonts/baskervville-v20-latin-regular.woff2",
+  variable: "--font-baskervville",
+  display: "swap",
+  weight: "400",
 })
 
-export const merriweather = Merriweather({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-merriweather',
-  weight: '400',
+export const merriweather = localFont({
+  src: "../public/fonts/merriweather-v33-latin-regular.woff2",
+  variable: "--font-merriweather",
+  display: "swap",
+  weight: "400",
 })
 
 export const taviraj = localFont({
-  src: '../public/fonts/Taviraj-Regular.ttf',
-  variable: '--font-taviraj',
+  src: "../public/fonts/Taviraj-Regular.ttf",
+  variable: "--font-taviraj",
+  display: "swap",
 })
-
-// export const corben = localFont({
-//   src: '../public/fonts/Corben-Regular.ttf',
-//   variable: '--font-corben',
-// })
